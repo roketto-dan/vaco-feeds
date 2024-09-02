39d167bccba7263627061b066f06f841efab6936
Youjin-cmd <y.kimiau@gmail.com>
2024-09-03 01:21:05 +0900

    install astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), icon()]
 });
