5edfba97f83bdd72c1b9c9d8a9454c2afe7202d6
Jack <jack@hopsoffice.com>
2024-09-10 19:53:14 +0900

    notion post 날짜 형식이 안맞는 문제 수정

--- src/data/notionPost.ts
+++ src/data/notionPost.ts
@@ -2,7 +2,7 @@ export const notionPosts = [
   {
     title: "올인원 테이블 제작기",
     link: "https://0jaaack.notion.site/b2a981df3d474476ad7328644971108f",
-    date: "2024-04-07",
+    date: "2024.04.07",
     user: "공재혁",
   },
 ];
