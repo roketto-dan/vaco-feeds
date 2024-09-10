f09714e809905e34919eac16e16467694e2f559a
Jack <jack@hopsoffice.com>
2024-09-10 20:08:50 +0900

    프로필을 클릭하거나 마우스를 대면 상세 정보가 뜨도록 수정

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { users, type User } from "../data/user";
 
 type ProfileProps = {
@@ -7,11 +8,53 @@ type ProfileProps = {
 export function Profile(props: ProfileProps) {
   const { userName } = props;
 
+  const [status, setStatus] = useState<"close" | "hover" | "open">("close");
+
   const user = users.find((user) => user.name === userName);
 
+  const handleProfileEnter = () => {
+    setStatus("hover");
+    setTimeout(() => {
+      setStatus((status) => {
+        return status === "hover" ? "open" : status;
+      });
+    }, 1000 * 0.5);
+  };
+
+  const handleProfileLeave = () => {
+    setStatus("close");
+  };
+
+  const handleButtonClick = () => {
+    setStatus((status) => {
+      switch(status) {
+        case "close":
+          return "open";
+        case "hover":
+          return "hover";
+        case "open":
+          return "close";
+      }
+    });
+    setTimeout(() => {
+      setStatus((status) => {
+        return status === "open" ? "close" : status;
+      });
+    }, 1000 * 3);
+  };
+
   return (
-    <span className="text-sm text-gray-700">
-      {user?.name ?? userName}
-    </span>
+    <div className="relative" onMouseEnter={handleProfileEnter} onMouseLeave={handleProfileLeave}>
+      <button className="flex items-center gap-1 text-sm text-gray-700" onClick={handleButtonClick}>
+        {user?.name ?? userName}
+      </button>
+      {user != null && status === 'open' && (
+        <div className="absolute top-full left-0 pt-1.5 z-10">
+          <section className="flex flex-col gap-4 p-4 w-52 rounded bg-white" style={{ boxShadow: '0px 0px 0px 1px #d1d9e080, 0px 6px 12px -3px #25292e0a, 0px 6px 18px 0px #25292e1f' }}>
+            {user.name}
+          </section>
+        </div>
+      )}
+    </div>
   );
 }
