4c1184d74e576481d7cb8fcc7cab4ccfb7a19767
Jack <jack@hopsoffice.com>
2024-09-10 19:54:09 +0900

    아스트로에 리액트 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon(), react()]
 });
