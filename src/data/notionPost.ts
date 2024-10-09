7d7612d6cc428a5699824c86e6e004926779714b
Myungjae Jang <laranhee@gmail.com>
2024-10-10 00:29:49 +0900

    노션 포스트 추가

--- src/data/notionPost.ts
+++ src/data/notionPost.ts
@@ -5,4 +5,10 @@ export const notionPosts = [
     date: "2024.04.07",
     user: "공재혁",
   },
+  {
+    title: "내 README.md에 목차를 넣는건 계획대로 되지 않아",
+    link: "https://oddvoyage.notion.site/README-md-119cfbbeec27803ea770c2c2ee3b2953",
+    date: "2024.10.10",
+    user: "장명재",
+  },
 ];
