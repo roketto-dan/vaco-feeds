4e8a55a889e6afc2009e86981cebb0a9822a6e6b
0jaaack <jack@hopsoffice.com>
2025-01-05 18:31:43 +0900

    썸네일 이미지가 캐싱될 수 있도록 수정

--- src/pages/thumbnails/[postId].webp.ts
+++ src/pages/thumbnails/[postId].webp.ts
@@ -36,6 +36,7 @@ export const GET: APIRoute = async ({ request }) => {
       headers: {
         "Content-Type": "image/webp",
         "Cache-Control": `public, max-age=${60 * 60}, s-maxage=${60 * 60 * 24 * 365}`,
+        "Content-Length": thumbnail.length.toString(),
         ETag: btoa(thumbnailUrl),
       },
     });
